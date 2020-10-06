c6e709e9f1a854ad018137c18f7cc8d99efed117
frankchoongsaeng <frankchoongsaeng@gmail.com>
2020-10-06 14:04:42 +0000

    Spacer Component Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Spacer from "./Spacer";
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Spacer />
     </div>
   );
 }
